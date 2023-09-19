a6655bd2a424e03e948e46f6b962c0f692e20faf
Vignesh-2623 <133001748+Vignesh-2623@users.noreply.github.com>
2023-09-19 12:08:30 +0530

    Add files via upload

---
+++ gettingStartedWithjs.js
@@ -0,0 +1,11 @@
+console.log("Getting Started With JS");
+console.warn("Hello,How arer you");
+console.error("ERRORR");
+
+var age;
+var bag="BOOKS";
+var fullname ='Vignesh';
+bag="Tiffin Box";
+console.log(bag);
+console.log(age);
+console.log(fullName);
